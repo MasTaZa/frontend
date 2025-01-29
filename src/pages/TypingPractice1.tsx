ae2d45e55e3368576140c31001bc5a1252f69995
hjzhhhj <jhj090120@gmail.com>
2025-01-29 16:05:53 +0900

    미림 타자연습

--- src/pages/TypingPractice1.tsx
+++ src/pages/TypingPractice1.tsx
@@ -16,15 +16,20 @@ const TypingPractice1 = () => {
                 <p>정확도 : 40.00%</p><br></br>
                 <img src="/img/mirim.png" alt="Mirim Logo"></img>
             </div>
+            <div className="VLine"></div>
             <div className="taza">
                 <div className="Ex">
                     <p>public class Main{'{'}</p> {/* "{" 를 쓰려면 이렇게 해야된다는 사실....... 귀한자식이구나 너 */}
+                    <p className="HLine"></p>
                     <p>public class Ms</p>
                 </div>
                 <div className="NextTaza">
                     <p>pubic static void main{'{'}String args[]{'}'}{'{'}</p>
                     <p>int a = 1; int b = 2; int c = 3;</p>
                     <p>System.out.println(a + b + c);</p>
+                </div>
+                <div className="Keyboard">
+                    
                 </div>
             </div>
         </div>
