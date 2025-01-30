222963cf90edf47975b5c1a3730c429af8beb639
hjzhhhj <jhj090120@gmail.com>
2025-01-30 20:36:40 +0900

    미림 타자연습 :: 키보드 구현미림 타자연습 :: 키보드 구현

--- src/pages/TypingPractice.tsx
+++ src/pages/TypingPractice.tsx
@@ -2,48 +2,108 @@ import React from "react";
 import "./TypingPractice.css";
 
 const TypingPractice = () => {
-  return (
-    <div className="container">
-      <div className="main-box">
-        {/*왼쪽*/}
-        <div className="info-panel">
-          <h2 className="title">낱말연습</h2>
-          <p>진행시간 : 00:00</p>
-          <h3>전체</h3>
-          <p>평균타수 : 100</p>
-          <p>정확도 : 100.00%</p>
-          <h3>현재</h3>
-          <p>최고타수 : 300</p>
-          <p>평균타수 : 200</p>
-          <p>정확도 : 40.00%</p>
-          <div className="logo-container">
-          <img src="/img/mirim.png" alt="Mirim Logo" className="logo" />
-          </div>
+    return (
+        <div className="fullScreen">
+            <div className="leftPanel">
+                <h3>낱말연습</h3>
+                <p>진행시간 : 00:00</p><br></br>
+                <h4>전체</h4>
+                <p>평균타수 : 100</p>
+                <p>정확도 : 100.00%</p><br></br>
+                <h4>현재</h4>
+                <p>최고타수 : 300</p>
+                <p>평균타수 : 200</p>
+                <p>정확도 : 40.00%</p><br></br>
+                <img src="/img/mirim.png" alt="Mirim Logo"></img>
+            </div>
+            <div className="VLine"></div>
+            <div className="taza">
+                <div className="Ex">
+                    <p>public class Main{'{'}</p> {/* "{" 를 쓰려면 이렇게 해야된다는 사실....... 귀한자식이구나 너 */}
+                    <p className="HLine"></p>
+                    <p>public class Ms</p>
+                </div>
+                <div className="NextTaza">
+                    <p>pubic static void main{'{'}String args[]{'}'}{'{'}</p>
+                    <p>int a = 1; int b = 2; int c = 3;</p>
+                    <p>System.out.println(a + b + c);</p>
+                </div>
+                <div className="Keyboard">
+                    <p className="progress"></p>
+                    <div className="keyboard-row">
+                        <p className="key">`</p>
+                        <p className="ckey">1</p>
+                        <p className="ckey">2</p>
+                        <p className="ckey">3</p>
+                        <p className="ckey">4</p>
+                        <p className="ckey">5</p>
+                        <p className="ckey">6</p>
+                        <p className="ckey">7</p>
+                        <p className="ckey">8</p>
+                        <p className="ckey">9</p>
+                        <p className="ckey">0</p>
+                        <p className="key">-</p>
+                        <p className="key">=</p>
+                        <p className="key">Backspace</p>
+                    </div>
+                    <div className="keyboard-row">
+                        <p className="key">Tab</p>
+                        <p className="ckey">Q</p>
+                        <p className="ckey">W</p>
+                        <p className="ckey">E</p>
+                        <p className="ckey">R</p>
+                        <p className="ckey">T</p>
+                        <p className="ckey">Y</p>
+                        <p className="ckey">U</p>
+                        <p className="ckey">I</p>
+                        <p className="ckey">O</p>
+                        <p className="ckey">P</p>
+                        <p className="ckey">[</p>
+                        <p className="ckey">]</p>
+                        <p className="key">\</p>
+                    </div>
+                    <div className="keyboard-row">
+                        <p className="key">CapsLock</p>
+                        <p className="ckey">A</p>
+                        <p className="ckey">S</p>
+                        <p className="ckey">D</p>
+                        <p className="ckey">F</p>
+                        <p className="ckey">G</p>
+                        <p className="ckey">H</p>
+                        <p className="ckey">J</p>
+                        <p className="ckey">K</p>
+                        <p className="ckey">L</p>
+                        <p className="ckey">;</p>
+                        <p className="ckey">‘</p>
+                        <p className="key">Enter</p>
+                    </div>
+                    <div className="keyboard-row">
+                        <p className="key">Shift</p>
+                        <p className="ckey">Z</p>
+                        <p className="ckey">X</p>
+                        <p className="ckey">C</p>
+                        <p className="ckey">V</p>
+                        <p className="ckey">B</p>
+                        <p className="ckey">N</p>
+                        <p className="ckey">M</p>
+                        <p className="ckey">,</p>
+                        <p className="ckey">.</p>
+                        <p className="ckey">/</p>
+                        <p className="key">Shift</p>
+                    </div>
+                    <div className="keyboard-row">
+                        <p className="key">Ctrl</p>
+                        <p className="key">Fn</p>
+                        <p className="key">Window</p>
+                        <p className="key">Alt</p>
+                        <p className="ckey">Space</p>
+                        <p className="key">한/영</p>
+                        <p className="key">한자</p>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        {/*오른쪽*/}
-        <div className="typing-area">
-          <div className="code-box">
-            <p className="code">public class Main{'{'}</p>
-            <p className="code error">public class <span className="highlight">Ms</span></p>
-          </div>
-
-          <div className="code-box">
-            <pre className="code">
-{`public static void main(String[] args){
-    int a = 1; int b = 2; int c = 3;
-    System.out.println(a + b + c);
-}`}
-            </pre>
-          </div>
-
-          <div className="keyboard-box">
-            {}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
 export default TypingPractice;
