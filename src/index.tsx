6e9e4f91f6b6439fb46fafcc57d466a43fa76ff0
hjzhhhj <jhj090120@gmail.com>
2025-02-05 23:43:48 +0900

    미림 타자연습 :: 랭킹 페이지 상단 패널 완성

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Ranking2 from './pages/Ranking2'
+import TypingPractice from './pages/TypingPractice'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
--- src/pages/Ranking2.tsx
+++ src/pages/Ranking2.tsx
@@ -5,8 +5,9 @@ const Ranking2 = () => {
     return (
         <div className="fullScreen">
             <div className="top-panel">
-                <img src="/img/mirim.png" alt="Mirim Logo"></img>
+                <img src="/img/mirim.png" className="mirim-png" alt="Mirim Logo"></img>
                 <p>타자연습</p>
+                <img src="/img/human.png" className="human-png" alt="human"></img>
             </div>
         </div>
     )
